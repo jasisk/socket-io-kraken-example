af781548cdec69e433caf8c166ac546fde90a7f9
Jean-Charles Sisk <jasisk@gmail.com>
2015-03-24 17:42:00 -0400

    cleanup

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 var path = require('path');
 
 module.exports = function (router) {
-  var viewCount = 0;
   router.get('/', function (req, res) {
-    var io = req.app.kraken.get('io');
     var filePath = path.join(__dirname, 'public/template.html');
     res.sendFile(filePath);
   });
